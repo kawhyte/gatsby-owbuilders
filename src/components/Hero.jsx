e8777a911990a37b04e6a80d285b6515ec8578b3
Kenny <kawhyte@gmail.com>
2020-08-03 14:57:34 -0700

    animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,5 @@
 import React from "react"
 import { Link } from "gatsby"
-import Img from "gatsby-image"
 import pool from "../images/house.jpg"
 
 function Hero() {
@@ -9,20 +8,7 @@ function Hero() {
     <div className="lg:relative overflow-hidden bg-blue-100">
     <div className="container mx-auto bg-blue-100 ">
       <div className="relative z-10 pb-8  sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
-       {/* <svg className="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2" fill="currentColor" viewBox="0 0 100 100" preserveAspectRatio="none">
-          <polygon points="50,0 100,0 50,100 0,100" />
-        </svg>
-  
-        {/* <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
-           <nav className="relative flex items-center justify-between sm:h-10 lg:justify-start">
-      
 
-           </nav>
-  </div> */}
-  
-
-
-  
         <main className="bg-white py-20 mt-10 mx-auto px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
           <div className="sm:text-center lg:text-left">
             <h2 className="text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl">
@@ -50,10 +36,7 @@ function Hero() {
       </div>
     </div>
     <div className="absolute lg:inset-y-0 lg:right-0 lg:w-1/2 ">
-      <img className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full" src={pool} alt="" />
-   
-   
-   
+      <img className="kenburns-top h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full" src={pool} alt="" />
    
       </div>
   </div>
@@ -63,26 +46,4 @@ function Hero() {
 
 export default Hero
 
-
-export const query = graphql`
-  query HeroQuery {
-    portfolio: allContentfulPortfolio {
-      nodes {
-        ...PortfolioCard
-      }
-    }
-
-    house: file(relativePath: { eq: "house.jpg" }) {
-      childImageSharp {
-        fluid(maxWidth: 600, maxHeight: 480, quality: 85) {
-          ...GatsbyImageSharpFluid_withWebp
-        }
-      }
-    }
-
-
-
-
-  }
-`
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,21 @@
 import { graphql } from "gatsby"
 import React from "react"
-import Cards from "../components/Cards"
 import Hero from "../components/Hero"
 import Layout from "../layouts/Layout"
-
-
 import SiteMetadata from "../components/SiteMetadata"
-
 import WhatWeDoSection from "../components/WhatWeDoSection"
 import Services from "../components/Services"
-import Projects from "../components/Projects"
 
 const IndexPage = ({ data }) => {
   return (
     <Layout>
       <SiteMetadata title="Home" description="Portfolio of OW Builders" />
- 
-      <Hero />
-      <WhatWeDoSection /> 
-       
-      <Services />
-  
 
-      
-      
-   
-      </Layout>
+      <Hero />
+      <WhatWeDoSection />
+
+      <Services />
+    </Layout>
   )
 }
 
