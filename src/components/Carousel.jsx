2b25d38723dc5d3e19edad2b4a6d0ea975627001
Kenny <kawhyte@gmail.com>
2020-08-03 16:58:36 -0700

    gallery updated

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -8,11 +8,21 @@ import "./Carousel.css"
 
 export const Carousel = ({ images }) => {
   const swiperParams = {
-    pagination: {
-      el: ".swiper-pagination",
-      type: "bullets",
-      clickable: true,
+   
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false
     },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    }
   }
   return (
     <Swiper {...swiperParams}>
