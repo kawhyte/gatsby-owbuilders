1225f993490ec072333cf0fc505e556bdc5f3255
Kenny Whyte <kawhyte@gmail.com>
2020-12-16 11:21:57 -0800

    test

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,7 +17,7 @@ function Hero() {
               <span className="text-red-400"> Project Management </span>
             </h2>
             <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-            Make your dream residential home or commercial building a reality.
+            Make your dream residential home or commercial building a reality
             </p>
             <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
               <div className="rounded-md shadow">
