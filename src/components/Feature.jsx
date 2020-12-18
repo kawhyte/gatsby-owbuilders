91d4fab5f7f8691dd031941ccfecd43faba87ba1
Kenny Whyte <kawhyte@gmail.com>
2020-12-18 11:12:13 -0800

    feature

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -3,16 +3,16 @@ import React from "react"
 function Feature() {
   return (
     <div>
-      <div class="py-12 bg-white">
+      <div class="py-8 bg-white">
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div class="lg:text-center">
-            <h2 class="text-base text-red-700 font-semibold tracking-wide uppercase">
+            <h2 class="text-sm sm:text-base text-red-700 font-semibold tracking-wide uppercase">
               What You will get
             </h2>
             <p class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
               Unique Architectural solutions
             </p>
-            <p class="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
+            <p class="mt-4 max-w-2xl text-base text-gray-500 lg:mx-auto">
               Lorem ipsum dolor sit amet consect adipisicing elit. Possimus
               magnam voluptatum cupiditate veritatis in accusamus quisquam.
             </p>
